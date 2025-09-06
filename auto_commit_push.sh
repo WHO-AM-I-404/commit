5f68be4b21c877141a41aeccc8b826a861afa1be
Who Am I <davaniko40@gmail.com>
2025-09-06 15:37:46 +0700

    Create auto_commit_push.sh

---
+++ auto_commit_push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Loop bikin 20 commit
+for i in {1..20}
+do
+    echo "Commit ke-$i" >> README.md   # bikin perubahan biar ada beda
+    git add README.md
+    git commit -m "Auto commit ke-$i"
+done
+
+# Push ke branch main
+git push origin main
